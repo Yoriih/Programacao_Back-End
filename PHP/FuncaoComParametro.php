1f21d890413a17fed6788c9696909a49a6bd2c58
Nicolas Gonçalves <61568561+Yoriih@users.noreply.github.com>
2025-10-20 21:22:32 -0300

    Add PHP function to sum two numbers

---
+++ PHP/FuncaoComParametro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function somar($valor1, $valor2)
+{
+$resultado = $valor1 + $valor2;
+echo "A soma de {$valor1} + {$valor2} é: {$resultado}";
+}
+somar(10, 5);
+?>
+
+</body>
+</html>
